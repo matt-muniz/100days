816b5a7789e3f8e63370c23aef11eec1e0fa8947
matt-muniz <mmunizsi01@gmail.com>
2020-03-30 13:33:15 -0400

    DAY 6

--- project1-blog/server/api/logs.js
+++ project1-blog/server/api/logs.js
@@ -1,13 +1,32 @@
 const { Router } = require('express');
 const router = Router();
 
+const LogEntrty = require('../models/LogComments');
+
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '968835',
+  key: '6a33f2e6e4b3290f9b48',
+  secret: 'e4f6b266d4be27d9adb9',
+  cluster: 'us2',
+});
+
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World!',
+  LogEntrty.find().exec((err, comments) => {
+    res.json({ comments });
   });
 });
-router.post('/', (req, res) => {
-  console.log(req.body);
+
+router.post('/', async (req, res, next) => {
+  try {
+    const logEntry = new LogEntrty(req.body);
+    pusher.trigger('Blog-Comments', 'new-event', logEntry);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- project1-blog/server/app.js
+++ project1-blog/server/app.js
@@ -24,26 +24,12 @@ app.use(
   })
 );
 
-var Pusher = require('pusher');
-
-var pusher = new Pusher({
-  appId: '968835',
-  key: '6a33f2e6e4b3290f9b48',
-  secret: 'e4f6b266d4be27d9adb9',
-  cluster: 'us2',
-});
-
-const port = process.env.PORT || 5000;
-
-app.post('/', (req, res) => {
-  pusher.trigger('Blog-Comments', 'new-event', req.body);
-  res.status(200).send();
-});
-
 app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 
 app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
